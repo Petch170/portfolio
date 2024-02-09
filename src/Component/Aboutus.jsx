e9f593477a9a04f321005fc237652d5c36f14217
Your Name <you@example.com>
2024-02-09 14:41:32 +0700

    port

--- src/Component/Aboutus.jsx
+++ src/Component/Aboutus.jsx
@@ -9,7 +9,7 @@ const Aboutus = () => {
         <p className=" text-sm md:text-md lg:text-lg font-base text-center text-wrap pt-2 ">
           I'm a career changer passionate about building responsive web
           applications. I graduated from the "Generation thailand" Bootcamp
-          cohort 6, mastering JavaScript, React, Node.js, Express, TailwindCSS,
+          cohort 6, master-ding JavaScript, React, Node.js, Express, TailwindCSS,
           MongoDB, HTML, CSS, and Git. Quick learner, always evolving. Excited to contribute my skills to innovative projects and continually
           grow in the ever-changing tech landscape.
         </p>
