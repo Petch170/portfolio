b91baad743de86c0636748330454931a4d327ce4
Your Name <you@example.com>
2024-02-10 16:10:36 +0700

    update 1

--- src/Component/Aboutus.jsx
+++ src/Component/Aboutus.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import Education from "./Education";
+
 
 const Aboutus = () => {
   return (
-<<<<<<< HEAD
     <div id="about-me" className="p-5 md:mx-36 lg:mx-56 md:h-[750px]">
       <h1 className="text-center text-lg md:text-xl lg:text-3xl font-semibold my-2 md:my-8 underline lg:pt-36">
         About
@@ -11,30 +9,20 @@ const Aboutus = () => {
       <div className=" flex flex-col md:flex-row gap-10 md:gap-4 ">
         <div className="flex flex-1">
           <p className="text-sm md:text-md lg:text-xl font-base text-start pt-2 px-8 ">
-            My name is <span className="font-bold"> Sojirat Petchpaiboon, </span>
-            I graduated from the "Generation thailand" Bootcamp cohort 6,
+            My name is{" "}
+            <span className="font-bold"> Sojirat Petchpaiboon, </span>I
+            graduated from the "Generation thailand" Bootcamp cohort 6,
             masterring JavaScript, React, Node.js, Express, TailwindCSS,
             MongoDB, HTML, CSS, and Git. Quick learner, always evolving. Excited
             to contribute my skills to innovative projects and continually grow
             in the ever-changing tech landscape.
           </p>
         </div>
-        <div className="flex flex-1 justify-center"><img src="/com.jpg"  className=" rounded-md h-[80%]"></img></div>
+        <div className="flex flex-1 justify-center">
+          <img src="/com.jpg" className=" rounded-md h-[80%]"></img>
+        </div>
         {/* <Education/> */}
       </div>
-=======
-    <div id="about-me" className="p-5 ">
-      {/* <div className="bg-red-300  border"> */}
-        <h1 className="text-center text-lg md:text-xl lg:text-2xl font-semibold ">About</h1>
-        <p className=" text-sm md:text-md lg:text-lg font-base text-center text-wrap pt-2 ">
-          I'm a career changer passionate about building responsive web
-          applications. I graduated from the "Generation thailand" Bootcamp
-          cohort 6, master-ding JavaScript, React, Node.js, Express, TailwindCSS,
-          MongoDB, HTML, CSS, and Git. Quick learner, always evolving. Excited to contribute my skills to innovative projects and continually
-          grow in the ever-changing tech landscape.
-        </p>
-      {/* </div> */}
->>>>>>> e9f593477a9a04f321005fc237652d5c36f14217
     </div>
   );
 };
